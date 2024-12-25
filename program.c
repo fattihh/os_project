880abaaea37ee68b2ba20386775f578539b98612
Eren Can Şahin <eren.sahin10@ogr.sakarya.edu.tr>
2024-12-26 01:55:48 +0300

    Boru (pipe) Fonksiyonları eklendi

--- program.c
+++ program.c
@@ -1,7 +1,7 @@
 #include "program.h"
 
-#define BUFFER_SIZE 1024
 #define TOK_DELIM " \t\r\n\a"
+
 
 // Global değişkenler
 char* currentDirectory;
@@ -28,13 +28,13 @@ int num_builtins() {
 Komut satırı prompt'unu gösteren fonksiyon
 */
 void display_prompt() {
-    char hostn[1204] = "";
+    char hostn[256] = "";
     gethostname(hostn, sizeof(hostn));
     if (getcwd(currentDirectory, 1024) == NULL) {
         perror("osprojectsh");
         strcpy(currentDirectory, "?");
     }
-    printf(KNRM "%s@%s:" KWHT KBLU "%s > " KWHT, getenv("LOGNAME"), hostn, currentDirectory);
+    printf(KNRM "%s@%s:" KWHT KBLU "%s > " KWHT, getenv("USER"), hostn, currentDirectory);
 }
 
 /**
@@ -83,7 +83,7 @@ Yardım komutunu gerçekleştiren fonksiyon
 int shell_help(char **args) {
     int i;
     printf("İşletim Sistemleri Ödevi - OS Project Shell\n");
-    printf("%s\n", "Yerleşik Komutlar:");
+    printf("Yerleşik Komutlar:\n");
     for (i = 0; i < num_builtins(); i++) {
         printf("  %s\n", builtin_commands[i]);
     }
@@ -139,6 +139,172 @@ char **split_line(char *line) {
 }
 
 /**
+ * Komut satırını borulara göre bölen fonksiyon.
+ * @param line Komut satırı girişi.
+ * @param num_commands Komut sayısını döndürmek için kullanılan çıktı parametresi.
+ * @return Komutların dizi dizisi.
+ */
+char ***split_commands(char *line, int *num_commands) {
+    int bufsize = 10;
+    int position = 0;
+    char ***commands = malloc(bufsize * sizeof(char**));
+    char *command;
+    
+    if (!commands) {
+        fprintf(stderr, "osprojectsh: Bellek tahsisi başarısız\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    command = strtok(line, "|");
+    while (command != NULL) {
+        // Her komut için baştaki boşlukları temizle
+        while (*command == ' ') command++;
+        
+        // Komutu tokenlara ayır
+        commands[position++] = split_line(command);
+        
+        if (position >= bufsize) {
+            bufsize += 10;
+            commands = realloc(commands, bufsize * sizeof(char**));
+            if (!commands) {
+                fprintf(stderr, "osprojectsh: Bellek tahsisi başarısız\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        command = strtok(NULL, "|");
+    }
+    commands[position] = NULL;
+    *num_commands = position;
+
+    // Debug: Bölünmüş komutları yazdır
+    for (int i = 0; i < *num_commands; i++) {
+        printf("Split Command[%d]: '%s'\n", i+1, commands[i][0]);
+    }
+
+    return commands;
+}
+
+/**
+ * Boru (pipe) içeren komut satırlarını çalıştıran fonksiyon.
+ * @param commands Borulara ayrılmış komutların dizisi.
+ * @param num_commands Komut sayısı.
+ * @return 1 Her zaman başarılı olarak döner.
+ */
+int execute_piped_commands(char ***commands, int num_commands) {
+    int i;
+    pid_t pid;
+    int in_fd = 0; // İlk komut için standart giriş
+    int fd[2];
+    pid_t *pids = malloc(num_commands * sizeof(pid_t));
+
+    if (!pids) {
+        fprintf(stderr, "osprojectsh: Bellek tahsisi başarısız\n");
+        return 1;
+    }
+
+    for (i = 0; i < num_commands; i++) {
+        if (i < num_commands - 1) {
+            // Her komut için bir pipe oluştur
+            if (pipe(fd) == -1) {
+                perror("pipe");
+                free(pids);
+                return 1;
+            }
+        }
+
+        pid = fork();
+        if (pid == 0) {
+            // Çocuk süreç
+
+            // Giriş yönlendirmesi
+            if (in_fd != 0) {
+                if (dup2(in_fd, STDIN_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(in_fd);
+            }
+
+            // Çıkış yönlendirmesi
+            if (i < num_commands - 1) {
+                close(fd[0]); // Okuma ucunu kapat
+                if (dup2(fd[1], STDOUT_FILENO) == -1) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+                close(fd[1]);
+            }
+
+            // Debug: Komut ve Argümanları Yazdırma
+            printf("Executing command %d: %s\n", i + 1, commands[i][0]);
+            for (int j = 0; commands[i][j] != NULL; j++) {
+                printf("  args[%d]: %s\n", j, commands[i][j]);
+            }
+
+            // Komutu yürüt
+            if (execvp(commands[i][0], commands[i]) == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid < 0) {
+            // Fork hatası
+            perror("fork");
+            free(pids);
+            return 1;
+        } else {
+            // Ebeveyn süreç
+            pids[i] = pid;
+
+            // Önceki giriş dosyasını kapat
+            if (in_fd != 0) {
+                close(in_fd);
+            }
+
+            // Çıkış dosyasını ebeveyn sürece aktar
+            if (i < num_commands - 1) {
+                close(fd[1]); // Yazma ucunu kapat
+                in_fd = fd[0]; // Sonraki komutun girişi için
+            }
+        }
+    }
+
+    // Ebeveyn süreç tüm çocuk süreçlerin bitmesini bekler
+    for (i = 0; i < num_commands; i++) {
+        int status;
+        waitpid(pids[i], &status, 0);
+        // Çıkış kodlarını burada işleyebilirsiniz
+    }
+
+    free(pids);
+    return 1;
+}
+
+int execute_external(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // Çocuk süreç: komutu yürüt
+        if (execvp(args[0], args) == -1) {
+            perror("execvp");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Hata durumu
+        perror("fork");
+    } else {
+        // Ebeveyn süreç: çocuğun bitmesini bekle
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+/**
  * Giriş yönlendirmesi ile birlikte harici komutları çalıştıran fonksiyon
  */
 int execute_external_with_input_redirection(char **args, char *input_file) {
@@ -162,12 +328,12 @@ int execute_external_with_input_redirection(char **args, char *input_file) {
 
         // Komutu yürüt
         if (execvp(args[0], args) == -1) {
-            perror("osprojectsh");
+            perror("execvp");
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
         // Hata durumu
-        perror("osprojectsh");
+        perror("fork");
     } else {
         // Ebeveyn süreç: çocuğun bitmesini bekle
         do {
@@ -190,7 +356,7 @@ int execute_external_with_output_redirection(char **args, char *output_file) {
         // Çocuk süreç: çıkış dosyasını stdout'a yönlendir
         int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (fd_out < 0) {
-            perror("Çıkış dosyası açılamadı");
+            perror("open");
             exit(EXIT_FAILURE);
         }
         if (dup2(fd_out, STDOUT_FILENO) == -1) {
@@ -202,12 +368,12 @@ int execute_external_with_output_redirection(char **args, char *output_file) {
 
         // Komutu yürüt
         if (execvp(args[0], args) == -1) {
-            perror("osprojectsh");
+            perror("execvp");
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
         // Hata durumu
-        perror("osprojectsh");
+        perror("fork");
     } else {
         // Ebeveyn süreç: çocuğun bitmesini bekle
         do {
@@ -231,12 +397,12 @@ int execute_external_background(char **args) {
         // Arka plan sürecinde terminali kontrol etmek istemiyorsanız, aşağıdaki satırı ekleyebilirsiniz:
         // setsid();
         if (execvp(args[0], args) == -1) {
-            perror("osprojectsh");
+            perror("execvp");
         }
         exit(EXIT_FAILURE);
     } else if (pid < 0) {
         // Hata durumu
-        perror("osprojectsh");
+        perror("fork");
     } else {
         // Ebeveyn süreç: arka plan sürecini listeye ekle
         bg_process *new_bg = malloc(sizeof(bg_process));
@@ -255,77 +421,50 @@ int execute_external_background(char **args) {
 }
 
 /**
- * Arka plan süreçlerini yakalayan sinyal işleyicisi
+ * Boru karakteri içerip içermediğini kontrol edip uygun şekilde çalıştıran fonksiyon.
+ * @param args Komut argümanları dizisi.
+ * @param line Orijinal komut satırı girişi.
+ * @return 1 Her zaman başarılı olarak döner.
  */
-void handle_sigchld(int sig) {
-    pid_t pid;
-    int status;
-
-    // Çocuk süreçlerin sonlanmasını kontrol et
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        // Arka plan sürecini listeden çıkar
-        bg_process **current = &bg_list;
-        while (*current) {
-            if ((*current)->pid == pid) {
-                bg_process *temp = *current;
-                *current = (*current)->next;
-                free(temp);
-                break;
-            }
-            current = &((*current)->next);
+int execute_command(char **args, char *line) {
+    // Boru karakteri içerip içermediğini kontrol et
+    int i;
+    int pipe_found = 0;
+    for (i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "|") == 0) {
+            pipe_found = 1;
+            break;
         }
-
-        // Exit kodunu al
-        int exit_code = 0;
-        if (WIFEXITED(status)) {
-            exit_code = WEXITSTATUS(status);
-        }
-
-        // Kullanıcıya bildir
-        printf("\n[%d] retval: %d\n", pid, exit_code);
-        display_prompt();
-        fflush(stdout);
-    }
-}
-
-
-int execute_external(char **args) {
-    pid_t pid, wpid;
-    int status;
-
-    pid = fork();
-    if (pid == 0) {
-        // Çocuk süreç: komutu yürüt
-        if (execvp(args[0], args) == -1) {
-            perror("osprojectsh");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        // Hata durumu
-        perror("osprojectsh");
-    } else {
-        // Ebeveyn süreç: çocuğun bitmesini bekle
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
-    return 1;
-}
+    if (pipe_found) {
+        // Boru bulundu, komut satırını borulara göre böl
+        // Orijinal komut satırını yeniden birleştirmek yerine, doğrudan borulara göre böl
+        int num_commands = 0;
+        // strdup ile orijinal line'ı kopyalayın çünkü strtok line'ı değiştirecek
+        char *line_copy = strdup(line);
+        if (!line_copy) {
+            fprintf(stderr, "osprojectsh: Bellek tahsisi başarısız\n");
+            return 1;
+        }
+        char ***commands = split_commands(line_copy, &num_commands);
 
-/**
- * Girilen komutu analiz eder ve uygun şekilde çalıştırır.
- * Giriş, Çıkış yönlendirmesi ve arka plan çalıştırma kontrolü eklenmiştir.
- */
-int execute_command(char **args) {
-    if (args[0] == NULL) {
-        // Boş komut girilmiş
-        return 1;
+        // Boru içeren komutları çalıştır
+        int status = execute_piped_commands(commands, num_commands);
+
+        // Belleği temizle
+        for (i = 0; i < num_commands; i++) {
+            free(commands[i]);
+        }
+        free(commands);
+        free(line_copy);
+
+        return status;
     }
 
+    // Boru yok, normal şekilde çalıştır
     // Arka plan çalıştırma kontrolü
     int background = 0;
-    int i;
     for (i = 0; args[i] != NULL; i++) {
         if (strcmp(args[i], "&") == 0) {
             background = 1;
@@ -387,7 +526,7 @@ int execute_command(char **args) {
             // Çıkış yönlendirmesi
             int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (fd_out < 0) {
-                perror("Çıkış dosyası açılamadı");
+                perror("open");
                 exit(EXIT_FAILURE);
             }
             if (dup2(fd_out, STDOUT_FILENO) == -1) {
@@ -399,12 +538,12 @@ int execute_command(char **args) {
 
             // Komutu yürüt
             if (execvp(args[0], args) == -1) {
-                perror("osprojectsh");
+                perror("execvp");
             }
             exit(EXIT_FAILURE);
         } else if (pid < 0) {
             // Hata durumu
-            perror("osprojectsh");
+            perror("fork");
         } else {
             // Ebeveyn süreç: çocuğun bitmesini bekle
             do {
@@ -443,6 +582,40 @@ void print_spaces() {
     printf("\n");
 }
 
+/**
+ * Arka plan süreçlerini yakalayan sinyal işleyicisi
+ */
+void handle_sigchld(int sig) {
+    pid_t pid;
+    int status;
+
+    // Çocuk süreçlerin sonlanmasını kontrol et
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        // Arka plan sürecini listeden çıkar
+        bg_process **current = &bg_list;
+        while (*current) {
+            if ((*current)->pid == pid) {
+                bg_process *temp = *current;
+                *current = (*current)->next;
+                free(temp);
+                break;
+            }
+            current = &((*current)->next);
+        }
+
+        // Exit kodunu al
+        int exit_code = 0;
+        if (WIFEXITED(status)) {
+            exit_code = WEXITSTATUS(status);
+        }
+
+        // Kullanıcıya bildir
+        printf("\n[%d] retval: %d\n", pid, exit_code);
+        display_prompt();
+        fflush(stdout);
+    }
+}
+
 int main(int argc, char **argv) {
     char *line = NULL;
     char **args;
@@ -479,7 +652,7 @@ int main(int argc, char **argv) {
         args = split_line(line);
 
         // Komutu yürüt
-        status = execute_command(args);
+        status = execute_command(args, line);
 
         // Belleği temizle
         free(args);
--- program.h
+++ program.h
@@ -1,35 +1,35 @@
 #ifndef PROGRAM_H
 #define PROGRAM_H
 
+#define _POSIX_C_SOURCE 200809L  // POSIX standartlarını etkinleştir
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>  // Çıkış dosyası açma için ekledi
-#include <signal.h> // Sinyal işleme için ekledi
+#include <fcntl.h>      // Çıkış dosyası açma için eklendi
+#include <signal.h>     // Sinyal işleyici için eklendi
 
 // Renk Kodları
-#define KNRM "\x1B[0m"          // Normal
-#define KWHT "\x1B[37m"         // Beyaz
-#define KBLU "\x1B[34m"         // Mavi
-#define KCYN "\x1B[36m"         // Camgöbeği
-#define _POSIX_C_SOURCE 200809L // POSIX standartları
+#define KNRM  "\x1B[0m"   // Normal
+#define KWHT  "\x1B[37m"  // Beyaz
+#define KBLU  "\x1B[34m"  // Mavi
+#define KCYN  "\x1B[36m"  // Camgöbeği
 
 // Arka Plan Süreç Yapısı
-typedef struct bg_process
-{
-    pid_t pid;               // Süreç ID'si
-    struct bg_process *next; // Sonraki süreç
+typedef struct bg_process {
+    pid_t pid;                  // Süreç ID'si
+    struct bg_process *next;    // Sonraki süreç
 } bg_process;
 
 // Global Değişkenler
-extern char *currentDirectory; // Geçerli Dizin
-extern bg_process *bg_list;    // Arka planda çalışan süreçler listesi
+extern char* currentDirectory;     // Geçerli Dizin
+extern bg_process *bg_list;        // Arka planda çalışan süreçler listesi
 
 // Yerleşik Komutlar ve Fonksiyonları
-extern char *builtin_commands[];            // Yerleşik komutlar dizisi
-extern int (*builtin_functions[])(char **); // Yerleşik komut fonksiyonları dizisi
+extern char *builtin_commands[];                        // Yerleşik komutlar dizisi
+extern int (*builtin_functions[])(char**);              // Yerleşik komut fonksiyonları dizisi
 
 // Fonksiyon Prototipleri
 
@@ -48,9 +48,10 @@ int shell_help(char **args);
 int shell_quit(char **args);
 
 // Yardımcı Fonksiyonlar
-char **split_line(char *line);     // Kullanıcı girdisini tokenlara ayırır.
+char **split_line(char *line); // Kullanıcı girdisini tokenlara ayırır.
+char ***split_commands(char *line, int *num_commands); // Komutları borulara göre böler
 int execute_external(char **args); // Yerleşik olmayan komutları harici olarak çalıştırır.
-int execute_command(char **args);  // Girilen komutu analiz eder ve uygun şekilde çalıştırır.
+int execute_command(char **args, char *line); // Girilen komutu analiz eder ve uygun şekilde çalıştırır.
 
 // Giriş ve Çıkış Yönlendirme Fonksiyonları
 int execute_external_with_input_redirection(char **args, char *input_file);
@@ -58,6 +59,12 @@ int execute_external_with_output_redirection(char **args, char *output_file);
 
 // Arka Plan Çalıştırma Fonksiyonu
 int execute_external_background(char **args);
+
+// Boru (pipe) Fonksiyonları
+int execute_piped_commands(char ***commands, int num_commands);
+
+// Signal Handler Fonksiyonu
+void handle_sigchld(int sig);
 
 // Diğer Yardımcı Fonksiyonlar
 void print_spaces();
